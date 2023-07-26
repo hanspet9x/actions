afd391c9b48eba6e94105a0487786c64357711a6
Peter A. Akinlolu <peterakinlolu1@gmail.com>
2023-07-26 12:03:57 +0100

    test file creation

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
+const path = require('path');
+
 
 try {
+    
     const email = core.getInput('email', {trimWhitespace: true, required: true});
     const password = core.getInput('password', {required: true});
     core.setOutput('logintime', {email, password, time: new Date().toLocaleDateString()});
+    fs.writeFileSync('./test.txt', 'Name=tolu')
 } catch (error) {
     core.debug(error.message);
     core.setFailed(error.message);
-}
+}d
